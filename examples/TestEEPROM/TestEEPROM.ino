dee8a0dc578837275de4c74602b7014316f09954
Lucas Inácio Viegas <lucas.viegas@edu.pucrs.br>
2022-07-12 21:37:06 -0300

    Included example for EEPROM read/write

---
+++ examples/TestEEPROM/TestEEPROM.ino
@@ -0,0 +1,38 @@
+#include <DS1624.h>
+
+DS1624 ds1624(7);
+void setup() {
+  Serial.begin(9600);
+
+  randomSeed(analogRead(A0));
+
+  static uint8_t dataOut[256] = { 0 };
+  static uint8_t dataIn[256] = { 0 };
+
+  // Fill the buffer with pseudo-random numbers
+  for (int i = 0; i < 256; ++i)
+    dataOut[i] = random(0, 256);
+
+  // Write them to EEPROM
+  if (ds1624.writeEEPROM(0, 256, dataOut) != 256) {
+    Serial.println("EEPROM write error!");
+    return;
+  }
+
+  // Read data back into another buffer
+  if (ds1624.readEEPROM(0, 256, dataIn) != 256) {
+    Serial.println("EEPROM read error!");
+    return;
+  }
+
+  // Check if buffers match
+  if (memcmp(dataOut, dataIn, 256) == 0) {
+    Serial.println("EEPROM write/read succesfull");
+  } else {
+    Serial.println("Invalid data read from EEPROM!");
+  }
+}
+
+void loop() {
+
+}
